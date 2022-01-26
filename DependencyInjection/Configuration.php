6e664a729ce8fd7e7ad5730777f03d386cc0e30f
TotalCRM <romanenko.a@bk.ru>
2022-01-26 17:18:59 +0300

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('totalcrm_excel');
+        $treeBuilder = new TreeBuilder('totalcrm_excel');
 
         return $treeBuilder;
     }
